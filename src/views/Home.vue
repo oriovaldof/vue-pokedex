e3bd3716028eddbb32ba41da48764fac0dc4f675
Oriovaldo Fialho <oriovaldof@gmail.com>
2022-09-30 21:58:01 -0400

    ajustes iniciais

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="row mt-2">
+      <div class="col mb-2">palco</div>
+      <div class="col mb-2">pokédex</div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  name: 'Home'
 }
 </script>
